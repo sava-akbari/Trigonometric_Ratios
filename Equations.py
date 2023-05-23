2f2b4f782569dbdc45be4a60a1b08047afb5d0c8
Sava Akbari <108176433+sava-akbari@users.noreply.github.com>
2023-05-23 13:05:50 +0330

    Update Equations.py

--- Equations.py
+++ Equations.py
@@ -1,1 +1,16 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import Radiobutton
+from math import sin, cos, tan, asin, acos, atan, radians
 
+# Define constants  
+#تعریف مقادیر ثابت
+WIDTH = 450 
+HEIGHT = 20
+
+# Create the main window
+# ایجاد کردن پنجره‌ی اصلی 
+root = tk.Tk()
+root.title("محاسبه‌ی نسبت‌های مثلثاتی با داشتن یک نسبت")
+canvas = tk.Canvas(root, width=WIDTH, height=HEIGHT) 
+canvas.pack() 
